9135c456d282e9d088295b7b502aaf20dd9c5fa4
KeruiHuang0 <56003891+KeruiHuang0@users.noreply.github.com>
2020-04-28 17:48:41 +0800

    Add files via upload
    Activities are the the labs that were supposed to be finished in the Lab.

---
+++ StudentInfo.java
@@ -0,0 +1,18 @@
+/**
+* Simple program to print StudentInfo. Description of your program goes here. *
+* Activity 1 (your activity or project number goes here).
+* @author Kerui Huang – COMP1210 - 007
+* @version 01/13/2020 */
+public class StudentInfo
+{
+/**
+* @param argsCommand line arguments
+*/
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Kerui Huang");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   Python");
+   }
+}
+
